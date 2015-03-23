a782f6507f6bd1dc21a3f6ec9568fb34b3b445c9
wrufesh <wrufesh@gmail.com>
2015-03-23 14:37:05 +0545

    string split instead of re.split

--- craigscrap/items.py
+++ craigscrap/items.py
@@ -9,7 +9,7 @@ import scrapy
 
 
 class Advertisement(scrapy.Item):
-    post_date = scrapy.Field()
+    post_datetime = scrapy.Field()
     post_title = scrapy.Field()
     post_detail_link = scrapy.Field()
     model_year = scrapy.Field()
--- craigscrap/spiders/craigspider.py
+++ craigscrap/spiders/craigspider.py
@@ -1,13 +1,12 @@
 import scrapy
-import re
 from craigscrap.items import Advertisement
 
 
 def make_url_list(raw_urls):
     new_urls = []
     for url in raw_urls:
-        splitted_url = re.split('/', url)
-        splitted_name = re.split('.', splitted_url[2])
+        splitted_url = url.split('/')
+        splitted_name = splitted_url[2].split('.')
         place_name = splitted_name[0]
         new_url = 'http://' + place_name + '.craigslist.org/search/cto?autoMakeModel=ford+ranger&autoMinYear=2005'
         new_urls.append(new_url)  
@@ -21,10 +20,19 @@ class CraigSpider(scrapy.Spider):
 
     def parse(self, response):
         sel = response.xpath('//div[@id="postingbody"]/blockquote/blockquote/ul/li/a/@href').extract()
-        # from scrapy.shell import inspect_response
-        # inspect_response(response)
-        
-        pass
-
+        item_urls = make_url_list(sel)
+        for item_url in item_urls:
+            yield scrapy.Request(item_url, self.parse_item)
+    
+    def parse_item(self, response):
+        item = Advertisement()
+        selectors = response.xpath('//div[@class="content"]/p[@class="row"]')
+        for sel in selectors:
+            item['post_datetime'] = sel.xpath('//span[@class="pl"]/time/@datetime').extract()[0]
+            item['post_title'] = sel.xpath('//span[@class="pl"]/a/text()')[0]
+            item['post_detail_link'] = sel.xpath('//span[@class="pl"]/a/@href').extract()[0]
+            from scrapy.shell import inspect_response
+            inspect_response(response)
+            yield item
 
    
