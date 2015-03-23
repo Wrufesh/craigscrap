2958f725b3923a6192a4535d4c114fcee3c53a31
wrufesh <wrufesh@gmail.com>
2015-03-23 15:38:14 +0545

    everything required scrapped

--- craigscrap/spiders/craigspider.py
+++ craigscrap/spiders/craigspider.py
@@ -24,6 +24,8 @@ class CraigSpider(scrapy.Spider):
         for item_url in item_urls:
             yield scrapy.Request(item_url, self.parse_item)
 
+    # in middle in case of many pages
+
     def parse_item(self, response):
         # from scrapy.shell import inspect_response
         # inspect_response(response)
@@ -33,8 +35,15 @@ class CraigSpider(scrapy.Spider):
         for sel in selectors:
             item['post_datetime'] = sel.xpath('//span[@class="pl"]/time/@datetime').extract()[0]
             item['post_title'] = sel.xpath('//span[@class="pl"]/a/text()').extract()[0]
-            item['post_detail_link'] = detail_link_prefix + sel.xpath('//span[@class="pl"]/a/@href').extract()[0]
+            detail_link = detail_link_prefix + sel.xpath('//span[@class="pl"]/a/@href').extract()[0]
+            item['post_detail_link'] = detail_link
             item['price'] = sel.xpath('//span[@class="l2"]/span[@class="price"]/text()').extract()[0]
-            yield item
+            # yield item
+            yield scrapy.Request(detail_link, self.parse_item_details, meta={'item': item})
 
-   
+    def parse_item_details(self, response):
+        # from scrapy.shell import inspect_response
+        # inspect_response(response)
+        item = response.meta['item']
+        item['model_year'] = response.xpath('//p[@class="attrgroup"]/span')[0].xpath('b/text()').extract()[0]
+        return item
