65a8747dfae55d6677fe06a7abe6307eee9a8ec2
wrufesh <wrufesh@gmail.com>
2015-03-25 22:10:07 +0545

    clean1

--- craigscrap/spiders/craigspider.py
+++ craigscrap/spiders/craigspider.py
@@ -4,30 +4,29 @@ import pdb
 from craigscrap.items import Advertisement
 
 
-def make_url_list():
-    places = ['sacramento', 'humboldt']
+def make_url_list(raw_urls):
     new_urls = []
-    for place_name in places:
-        # splitted_url = url.split('/')
-        # splitted_name = splitted_url[2].split('.')
-        # place_name = splitted_name[0]
-        new_url = 'http://webcache.googleusercontent.com/search?q=cache:' + place_name + '.craigslist.org/search/cto'
+    for url in raw_urls:
+        splitted_url = url.split('/')
+        splitted_name = splitted_url[2].split('.')
+        place_name = splitted_name[0]
+        new_url = 'http://' + place_name + '.craigslist.org/search/cto?autoMakeModel=ford+ranger&autoMinYear=2005'
         new_urls.append(new_url)
     return new_urls
 
 
-# def get_paginated_url(string_data, url):
-#     url_list = []
-#     x = string_data.split(' ')
-#     if len(x) == 5:
-#         total_items = int(x[4])
-#         total_loop = total_items/100
-#         for i in range(0, total_loop):
-#             v = str(i * 100)
-#             url_list.append(url + '&s=' + v)
-#     else:
-#         url_list.append(url)
-#     return url_list
+def get_paginated_url(string_data, url):
+    url_list = []
+    x = string_data.split(' ')
+    if len(x) == 5:
+        total_items = int(x[4])
+        total_loop = total_items/100
+        for i in range(0, total_loop):
+            v = str(i * 100)
+            url_list.append(url + '&s=' + v)
+    else:
+        url_list.append(url)
+    return url_list
 
 
 def to_datetime_object(date_str):
@@ -37,16 +36,15 @@ def to_datetime_object(date_str):
 class CraigSpider(scrapy.Spider):
     name = 'craigs'
     allowed_domains = ['craigslist.org']
-    # start_urls = ['http://webcache.googleusercontent.com/search?q=cache:geo.craigslist.org/iso/us/ca']
-    start_urls = make_url_list()
+    start_urls = ['http://geo.craigslist.org/iso/us/ca']
 
-    # def parse(self, response):
-    #     sel = response.xpath('//div[@id="postingbody"]/blockquote/blockquote/ul/li/a/@href').extract()
-    #     item_urls = make_url_list(sel)
-    #     for item_url in item_urls:
-    #         # pdb.set_trace()
-    #         yield scrapy.Request(item_url, self.parse_item)
-    #         # yield scrapy.Request(item_url, callback=self.pagination_check)
+    def parse(self, response):
+        sel = response.xpath('//div[@id="postingbody"]/blockquote/blockquote/ul/li/a/@href').extract()
+        item_urls = make_url_list(sel)
+        for item_url in item_urls:
+            # pdb.set_trace()
+            yield scrapy.Request(item_url, self.parse_item)
+            # yield scrapy.Request(item_url, callback=self.pagination_check)
 
     # in middle in case of many pages
     # def pagination_check(self, response):
@@ -59,7 +57,7 @@ class CraigSpider(scrapy.Spider):
     #         scrapy.log.msg(p_url)
     #         yield scrapy.Request(p_url, callback=self.parse_item)
 
-    def parse(self, response):
+    def parse_item(self, response):
         # from scrapy.shell import inspect_response
         # inspect_response(response)
         item = Advertisement()
